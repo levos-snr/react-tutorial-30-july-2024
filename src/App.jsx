680ce564649f6626cc558e1ca27f2e7827295ffa
Lewis Odero <lewisodero27@gmail.com>
2024-07-30 11:12:43 +0300

    init: starter kit for reactjs

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div>
+      <h1
+        className="text-center mt-7 text-2xl
+       s text-red-300 hover:cursor-pointer"
+      >
+        My React App Stater
+      </h1>
+    </div>
+  );
+};
+
+export default App;
